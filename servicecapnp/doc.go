09bc2b81d2c4f1e71b82b09b432fc5ec5e299420
IndAlok <152197161+IndAlok@users.noreply.github.com>
2026-03-27 19:19:06 +0530

    few cleanups and generated files removed (moved to meson build)

---
+++ servicecapnp/doc.go
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2026 RizinOrg <info@rizin.re>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+// Package servicecapnp contains generated Cap'n Proto bindings.
+//
+//go:generate go run generate.go
+package servicecapnp
